eb5c1e61fc1823972919f6b721faab9164e7df2a
Brian Douglass <bhdouglass@gmail.com>
2023-01-24 20:00:09 -0500

    Updated files from js to ts

--- src/utilities.ts
+++ src/utilities.ts
@@ -1,18 +1,21 @@
-export function titleCase(str) {
+import { MarkdownInstance } from "astro";
+import { Frontmatter } from "./types";
+
+export function titleCase(str: string) {
   return str.replace(/_/g, ' ').replace(/\w\S*/g, (txt) => {
     return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();
   });
 };
 
-export function tagTitleCase(tag) {
+export function tagTitleCase(tag: string) {
   return titleCase(tag.replace(/-/g, ' '));
 };
 
-export function categoriesToTags(categories) {
+export function categoriesToTags(categories?: string) {
   return (categories ?? '').trim().split(' ').map((category) => category.trim());
 }
 
-export function sortPosts(posts) {
+export function sortPosts(posts: MarkdownInstance<Frontmatter>[]) {
   if (process.env.NODE_ENV !== 'development') {
     posts = posts.filter((post) => !post.frontmatter.draft);
   }
@@ -21,7 +24,7 @@ export function sortPosts(posts) {
   return posts;
 }
 
-export function getRssLink(url, base) {
+export function getRssLink(url: string, base: string) {
   const rssUrl = new URL(`${url}/`, base);
   rssUrl.searchParams.append('utm_source', 'rss');
   rssUrl.searchParams.append('utm_medium', 'rss');
