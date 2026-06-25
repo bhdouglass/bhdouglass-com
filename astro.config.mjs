746e2a78d0d707a6ce8f57b097636ad7abe1c0bb
Brian Douglass <bhdouglass@gmail.com>
2026-06-25 13:23:05 -0400

    Removed usage of astro-m2dx

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,39 +5,26 @@ import vue from '@astrojs/vue';
 import robotsTxt from 'astro-robots-txt';
 import mdx from '@astrojs/mdx';
 import taxonomyDb from './.frontmatter/database/taxonomyDb.json';
-import m2dx from 'astro-m2dx';
 import pagefind from 'astro-pagefind';
 import tailwindcss from "@tailwindcss/vite";
-
 import og from 'astro-og';
-
-/** @type {import('astro-m2dx').Options} */
-const m2dxOptions = {
-  autoImports: true,
-  exportComponents: true,
-};
+import remarkAutoImports from './scripts/remark-auto-imports.mjs';
 
 const SITE = 'https://bhdouglass.com/';
 const categoryPages = taxonomyDb.taxonomy.categories.map((category) => `${SITE}blog/${category}/`);
 
-// https://astro.build/config
 export default defineConfig({
   site: SITE,
-  server: {
-    host: true,
-  },
+  server: { host: true },
   prefetch: true,
   markdown: {
     syntaxHighlight: "prism",
+    remarkPlugins: [remarkAutoImports, remarkToc],
   },
   integrations: [vue(), mdx(), sitemap({
     customPages: ["https://bhdouglass.com/resume/"],
     filter: (page) => !categoryPages.includes(page),
   }), robotsTxt({ sitemapBaseFileName: "sitemap" }), pagefind(), og()],
-  markdown: {
-    remarkPlugins: [[m2dx, m2dxOptions], remarkToc],
-  },
-
   vite: {
     plugins: [tailwindcss()],
   },
--- src/_components.ts
+++
@@ -1,7 +0,0 @@
-import Anchor from './components/Anchor.astro';
-import Image from './components/Image.astro';
-
-export const components = {
-  img: Image,
-  a: Anchor,
-};
